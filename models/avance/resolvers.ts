9dfb6924608aa6b75b149ca5429be53f2011672c
Marale9 <maralereales@gmail.com>
2021-12-03 19:40:53 -0500

    agregar mutaciones de avances

--- models/avance/resolvers.ts
+++ models/avance/resolvers.ts
@@ -12,6 +12,7 @@ const resolversAvance = {
         }).populate('proyecto').populate('creadoPor')
         return avanceFiltrado;
     }
+  
   },
 
   Mutation: {
@@ -19,13 +20,39 @@ const resolversAvance = {
           const avanceCreado = await modeloAvances.create({
             fecha: args.fecha,
             descripcion: args.descripcion,
-            observaciones: args.proyecto,
+            observaciones: args.observaciones,
             proyecto: args.proyecto,
             creadoPor: args.creadoPor,
           });
           return avanceCreado;
       },
-     
+
+      crearObservaciones: async(parent, args)=> {
+        const observacionCreada = await modeloAvances.findByIdAndUpdate(args._id,{
+          fecha: args.fecha,
+          descripcion: args.descripcion,
+          observaciones: args.observaciones,
+          proyecto: args.proyecto,
+          creadoPor: args.creadoPor,
+        });
+        return observacionCreada;
+    },
+    actualizarAvance: async(parent, args)=> {
+      const avanceActualizado = await modeloAvances.findByIdAndUpdate(args._id,{
+        fecha: args.fecha,
+        descripcion: args.descripcion,
+        observaciones: args.observaciones,
+        proyecto: args.proyecto,
+        creadoPor: args.creadoPor,
+      });
+      return avanceActualizado;
+  },
+  eliminarAvance: async (parent, args) => {
+    const avanceEliminado = await modeloAvances.findByIdAndDelete({
+      _id: args._id,
+    });
+    return avanceEliminado;
+  },
   },
 };
 export  {resolversAvance};
